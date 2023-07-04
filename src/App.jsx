f84dd6666791eeb8a6d8897a0a94ce24a0f6bc3e
dafnemus <dafnemus@gmail.com>
2023-07-04 16:07:01 -0300

    Task 2: - Created handleChange func to get input value - Created addNumber func to add a new number into array of numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Title from "./components/Title";
 import "./App.css";
+import Gather from "./components/Input";
+import { useState } from "react";
 
 function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    return setValue(e.target.value);
+  };
+
+  const addNumber = () => {
+    setNumbers([...numbers, value]);
+    setValue("");
+  };
+  console.log(numbers);
   return (
     <>
-      <Title text="Hola" />
+      <Title text="Agregar números" />
+      <Gather
+        value={value}
+        onChange={handleChange}
+        onClick={addNumber}
+        list={numbers}
+      />
     </>
   );
 }
